8e52fca4d57483ad2bad307da1ff14a104c9b653
anshumalivfx <anshumali.karna99@gmail.com>
2022-02-16 21:28:43 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Anshumali Karna on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
